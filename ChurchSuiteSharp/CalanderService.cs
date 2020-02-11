804b261533af3790af669a9bf1c0dbb9e230221d
David STRICKLAND <david.strickland@amadeus.com>
2020-02-11 00:08:41 +0000

    Added Services for Calander and SmallGroups

---
+++ ChurchSuiteSharp/CalanderService.cs
@@ -0,0 +1,16 @@
+﻿using ChurchSuiteSharp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class CalanderService
+{
+    public static async Task<IList<Calendar>> RetrieveCalanderAsync(HttpClient httpClient, string account)
+    {
+        var result = await httpClient.GetAsync($"https://{account}.churchsuite.co.uk/embed/calendar/json");
+        var resultString = await result.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<List<Calendar>>(resultString);
+    }
+}
---
+++ ChurchSuiteSharp/SmallGroupService.cs
@@ -0,0 +1,25 @@
+﻿using ChurchSuiteSharp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChurchSuiteSharp
+{
+    public class SmallGroupService
+    {
+        public static async Task<IList<SmallGroup>> RetrieveSmallGroups(string subDomain,HttpClient client)
+        {
+            var result = await client.GetAsync($"https://{subDomain}.churchsuite.co.uk/embed/smallgroups/json");
+            var resultString = await result.Content.ReadAsStringAsync();
+
+            var settings = new JsonSerializerSettings()
+            {
+                MissingMemberHandling = MissingMemberHandling.Error
+            };
+            return JsonConvert.DeserializeObject<List<SmallGroup>>(resultString, settings);
+        }
+    }
+}
